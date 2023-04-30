793626501a6d848595de93f9312222f038958694
K M Farhat Snigdah <farhatsnigdho1392@gmail.com>
2023-04-30 12:06:15 +0600

    Initial commit

---
+++ ATMApp/App/Entry.cs
@@ -0,0 +1,26 @@
+﻿using ATMApp.Domain.Interfaces;
+using ATMApp.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.AppScreen
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            UI.Screen.welcome();
+
+            ATMApp atmApp = new ATMApp();
+
+            atmApp.InitializeData();
+            atmApp.CheckUsercardNumAndPassword();
+            atmApp.Welcome();
+
+            Utility.PressEnterToContinue();
+        }
+    }
+}
---
+++ ATMApp/UI/Screen.cs
@@ -0,0 +1,54 @@
+﻿using ATMApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class Screen
+    {
+       internal static void welcome()
+       {
+            //Clear console, set title and color
+            Console.Clear();
+            Console.Title = "My ATM App";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //set wellcome message
+            Console.WriteLine("\n\n-----------------Welcome to my App----------------\n\n");
+            Console.WriteLine("Please insert your ATM Card");
+            Console.WriteLine("Node: Actual ATM machine will accept and validate");
+            Console.WriteLine("A physical ATM card, read the card number and validate it");
+
+            Utility.PressEnterToContinue();
+       }
+
+       internal static UserAccount UserLoginForm()
+       {
+            UserAccount tempUserAccount = new UserAccount();
+
+            tempUserAccount.CardNumber = Validator.Convert<long>("Your card Number");
+            tempUserAccount.CardPin = Convert.ToInt32(Utility.GetSecretInput("Enter your card PIN"));
+
+            return tempUserAccount;
+       }
+       
+       internal static void LoginProgress()
+       {
+            Console.WriteLine("\nChecking card number and PIN... ");
+            Utility.PrintDotAnimation();    
+       } 
+
+
+       internal static void PrintLockScreen()
+       {
+            Console.Clear();
+            Utility.PrintMessage("Your account is locked. Please go to the nearest branch." +
+                "to unclock your account. Thank You", false);
+            Utility.PressEnterToContinue();
+            Environment.Exit(1);
+        }
+    }
+}
